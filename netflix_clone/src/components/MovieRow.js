9694276431603ac0127984b14a557476425fad57
Manassés Queiroz <manassescruzgt@gmail.com>
2023-05-19 22:29:01 -0300

    Concluido100%

--- netflix_clone/src/components/MovieRow.js
+++ netflix_clone/src/components/MovieRow.js
@@ -1,24 +1,60 @@
-import React from "react";
+import React,{useState} from "react";
 import "./MovieRow.css";
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 
 export default ({ title, items }) => {
+
+  const[scrollX, setScrollX] = useState(0);
+
+  const handleLeftArrow = ()=>{
+    let x = scrollX + window.innerWidth / 2
+    if(x > 0){
+      x=0;
+    }
+    setScrollX(x)
+  }
+  const handleRightArrow = ()=>{
+    let x = scrollX - window.innerWidth / 2
+    let maxWidth =  items.results.length * 180
+    if((window.innerWidth - maxWidth) > x ){
+      x =  (window.innerWidth - maxWidth) -50
+    }
+    setScrollX(x)
+  }
+  
+console.log(items.results.leght)
   return (
     <div className="movieRow">
       <h2>{title}</h2>
-      <div className="movieRow--listarea">
-        {items.results.leght != 0 &&
-          items.results.map((movie, key) => (
-            <div className="movieRow--item" key={key}>
-              <img
-                src={
-                  movie.poster_path
-                    ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
-                    : "/image/imagem.jpg"
-                }
-                alt={`${movie.original_title}`}
-              />
-            </div>
-          ))}
+      
+        <div className="movieRow--left" onClick={handleLeftArrow}>
+          <NavigateBeforeIcon style={{fontSize:70}}/>
+        </div>
+        <div className="movieRow--right" onClick={handleRightArrow}>
+          <NavigateNextIcon style={{fontSize:70}}/>
+        </div>
+        <div className="movieRow--listarea">
+        <div className="movieRow--list" style={{
+          marginLeft:scrollX,
+          width: items.results.length * 180
+
+          
+        }}>
+          {items.results.leght != 0 &&
+            items.results.map((movie, key) => (
+              <div className="movieRow--item" key={key} >
+                <img
+                  src={
+                    movie.poster_path
+                      ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+                      : "/image/imagem.jpg"
+                  }
+                  alt={`${movie.original_title}`}
+                />
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
